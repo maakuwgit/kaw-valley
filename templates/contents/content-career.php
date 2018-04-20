bc9fd526541693f056a5eff546412bc418398d51
MarkW <mark@liftedlogic.com>
2018-04-20 12:01:20 -0500

    careers page mobile adjustments and breadcrumb

--- templates/contents/content-career.php
+++ templates/contents/content-career.php
@@ -3,11 +3,15 @@ $application = get_field('career_application');
 $contract = get_field('career_contract');
 if( $contract ) $contract = $application['url'];
 $location = get_field('career_location');
-if( $location ) $location = $location->post_name;
-var_dump($location);
+if( $location ) {
+  $state = get_the_terms($location->ID, 'state');
+  $location = $location->post_title;
+  if( $state ) $location .= ' ' . ucwords($state[0]->name);
+}
 ?>
 <figure class="content light-bg" id="hero-career" data-component="hero-banner">
   <div class="container row boxed">
+    <nav class="breadcrumb"><a href="">&lsaquo;&nbsp;Back to all job listings</a></nav>
     <div class="col-12of12">
       <h2 class="hero">We're Hiring.</h2>
       <p class="h1">Full time, salaried position in <?php echo $location; ?></p>
