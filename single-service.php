2d5e42a0eb257ec9b9304ca437ff9ab6a2a6f79e
MarkW <mark@liftedlogic.com>
2018-04-25 14:41:36 -0500

    Cleaned out all the unnecessary single files in favor of the classic get_post_type() method

--- single-service.php
+++
@@ -1,3 +0,0 @@
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/contents/content', 'service'); ?>
-<?php endwhile; ?>
--- single-team.php
+++
@@ -1,3 +0,0 @@
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/contents/content', 'team'); ?>
-<?php endwhile; ?>
