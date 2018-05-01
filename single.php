9c409d2b5c01ab54361c7c52f0335b445d001dcf
MarkW <mark@liftedlogic.com>
2018-05-01 13:38:49 -0500

    The logic I put in place to ensure that the 'single' template pulled the 'content-single' module and not 'content' was breaking all other pages. Some more complex if/then stuff fixed it

--- single.php
+++ single.php
@@ -1,3 +1,7 @@
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/contents/content', 'single'); ?>
+<?php
+  while (have_posts()) :
+    the_post();
+    $type = get_post_type();
+    if($type === 'post') $type = 'single';
+    get_template_part('templates/contents/content', $type); ?>
 <?php endwhile; ?>
