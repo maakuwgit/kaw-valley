f82bad30304906658e6878e24d765bf17ad1307b
MarkW <mark@liftedlogic.com>
2018-05-01 11:55:09 -0500

    Hero Banner spotlight logic fix.

--- archive-career.php
+++ archive-career.php
@@ -4,9 +4,10 @@ Template Name: Tabular
 */
 $cat = get_queried_object();
 
+$spotlight_strength = get_field( 'careers_hero_spotlight_strength', 'options' );
 $banner_text = array( 'text' => $cat->label . '.' );
 $banner_sub_text = false;
-$background_image = get_field( 'careers_hero_background_image', 'options');
+$background_image = get_field( 'careers_hero_background_image', 'options' );
 ?>
 <?php include( locate_template('templates/contents/content-hero.php') ); ?>
 <?php include( locate_template('templates/contents/content-archive__career.php') ); ?>
--- templates/contents/content-hero.php
+++ templates/contents/content-hero.php
@@ -1,9 +1,15 @@
 <?php
+  if( !$spotlight_strength ) {
+    $spotlight_strength = get_field( 'hero_banner_spotlight_strength' );
+  }
   if( !$banner_text ) $banner_text = get_field( 'hero_banner_main_text' );
   $next_post   = get_next_post( true );
   if( !$show_icons ) $show_icons  = get_field( 'hero_banner_show_icons' );
   $hero_banner = ll_format_post_banner();
 
+  if( $spotlight_strength ) {
+    $hero_banner['spotlight_strength'] = $spotlight_strength;
+  }
   if( $banner_text ) {
     $hero_banner['main_text']['text'] = $banner_text['text'];
   }
