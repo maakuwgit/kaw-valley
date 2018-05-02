22eea80efcde66d90b89e0aa06e0f2188b61e7da
MarkW <mark@liftedlogic.com>
2018-05-02 16:43:09 -0500

    Fix for Christina

--- lib/custom/custom-wysiwyg.php
+++ lib/custom/custom-wysiwyg.php
@@ -14,13 +14,13 @@ function ll_new_mce_button( $buttons ) {
 add_filter( 'mce_buttons_2', 'll_new_mce_button' );
 
 /**
- * Removes the random classes added by TinyMCE vß4
+ * Removes the random classes added by TinyMCE v4
  */
-function strip_tinymce_styles($in) {
+function ll_strip_tinymce_styles($in) {
   $in['paste_preprocess'] = "function(pl,o){ o.content = o.content.replace(/p class=\"p[0-9]+\"/g,'p'); o.content = o.content.replace(/span class=\"s[0-9]+\"/g,'span'); }";
   return $in;
 }
-add_filter('tiny_mce_before_init', 'strip_tinymce_styles');
+add_filter('tiny_mce_before_init', 'll_strip_tinymce_styles');
 
 /**
  * adds custom formats to the formats selection
@@ -30,15 +30,148 @@ add_filter('tiny_mce_before_init', 'strip_tinymce_styles');
  * @return array       Tinyce data
  */
 function ll_format_tinymce( $data ) {
-    $style_formats = array(
-
-      // array(
-      //   'title'    => 'Button',
-      //   'classes'  => 'btn',
-      //   'selector' => 'a',
-      //   'wrapper'  => false
-      // ),
-    );
+  $style_formats = array(
+    array(
+      'title'    => 'Heading Sizes',
+      'items'  => array(
+        array(
+          'title'    => 'Hero',
+          'classes'  => 'large-paragraph-text',
+          'selector' => 'h1, h2, h3, h4, h5, h6, p, a, span, li, time, dt, dd',
+          'wrapper'  => false
+        ),
+        array(
+          'title'    => 'Heading 1',
+          'classes'  => 'h1',
+          'selector' => 'h1, h2, h3, h4, h5, h6, p, a, span, li, time, dt, dd',
+          'wrapper'  => false
+        ),
+        array(
+          'title'    => 'Heading 2',
+          'classes'  => 'h2',
+          'selector' => 'h1, h2, h3, h4, h5, h6, p, a, span, li, time, dt, dd',
+          'wrapper'  => false
+        ),
+        array(
+          'title'    => 'Heading 3',
+          'classes'  => 'h3',
+          'selector' => 'h1, h2, h3, h4, h5, h6, p, a, span, li, time, dt, dd',
+          'wrapper'  => false
+        ),
+        array(
+          'title'    => 'Heading Style Four',
+          'classes'  => 'h4',
+          'selector' => 'h1, h2, h3, h4, h5, h6, p, a, span, li, time, dt, dd',
+          'wrapper'  => false
+        ),
+        array(
+          'title'    => 'Heading Style Five',
+          'classes'  => 'h5',
+          'selector' => 'h1, h2, h3, h4, h5, h6, p, a, span, li, time, dt, dd',
+          'wrapper'  => false
+        ),
+        array(
+          'title'    => 'Heading Style Six',
+          'classes'  => 'h6',
+          'selector' => 'h1, h2, h3, h4, h5, h6, p, a, span, li, time, dt, dd',
+          'wrapper'  => false
+        ),
+      ),
+    ),
+    array(
+      'title' => 'Images',
+      'items' => array(
+        array(
+          'title'    => 'Offset',
+          'classes'  => 'offset',
+          'selector' => 'img, figure',
+          'wrapper'  => false
+        ),
+        array(
+          'title'    => 'Invert',
+          'classes'  => 'invert',
+          'selector' => 'img, figure',
+          'wrapper'  => false
+        ),
+      ),
+    ),
+    array(
+      'title' => 'Buttons & Links',
+      'items' => array(
+        array(
+          'title'    => 'Button',
+          'classes'  => 'button',
+          'selector' => 'a, button',
+          'wrapper'  => false
+        ),
+        array(
+          'title'    => 'Hollow Button',
+          'classes'  => 'button hollow',
+          'selector' => 'a, button',
+          'wrapper'  => false
+        ),
+      ),
+    ),
+    array(
+        'title' => 'Colors',
+        'items' => array(
+          array(
+            'title'    => 'Orange',
+            'classes'  => 'orange',
+            'selector' => 'h1, h2, h3, h4, h5, h6, p, a, span, li, time, dt, dd, address, code',
+            'wrapper'  => false
+          ),
+          array(
+            'title'    => 'Red',
+            'classes'  => 'red',
+            'selector' => 'h1, h2, h3, h4, h5, h6, p, a, span, li, time, dt, dd, address, code',
+            'wrapper'  => false
+          ),
+          array(
+            'title'    => 'Chartreuese',
+            'classes'  => 'grey',
+            'selector' => 'h1, h2, h3, h4, h5, h6, p, a, span, li, time, dt, dd, address, code',
+            'wrapper'  => false
+          ),
+          array(
+            'title'    => 'Teal',
+            'classes'  => 'grey',
+            'selector' => 'h1, h2, h3, h4, h5, h6, p, a, span, li, time, dt, dd, address, code',
+            'wrapper'  => false
+          ),
+          array(
+            'title'    => 'Black',
+            'classes'  => 'black',
+            'selector' => 'h1, h2, h3, h4, h5, h6, p, a, span, li, time, dt, dd, address, code',
+            'wrapper'  => false
+          ),
+          array(
+            'title'    => 'Gray',
+            'classes'  => 'grey',
+            'selector' => 'h1, h2, h3, h4, h5, h6, p, a, span, li, time, dt, dd, address, code',
+            'wrapper'  => false
+          ),
+          array(
+            'title'    => 'Steel',
+            'classes'  => 'steel',
+            'selector' => 'h1, h2, h3, h4, h5, h6, p, a, span, li, time, dt, dd, address, code',
+            'wrapper'  => false
+          ),
+          array(
+            'title'    => 'Silver',
+            'classes'  => 'silver',
+            'selector' => 'h1, h2, h3, h4, h5, h6, p, a, span, li, time, dt, dd, address, code',
+            'wrapper'  => false
+          ),
+          array(
+            'title'    => 'White',
+            'classes'  => 'white',
+            'selector' => 'h1, h2, h3, h4, h5, h6, p, a, span, li, time, dt, dd, address, code',
+            'wrapper'  => false
+          ),
+        ),
+    )
+  );
 
   $data['style_formats'] = json_encode( $style_formats );
   return $data;
